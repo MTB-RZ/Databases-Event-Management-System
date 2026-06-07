d34eb5e154c36a6b1875312138b5a490d62e08a8
MTBRZ <muteebraza28@gmail.com>
2026-06-07 22:09:25 +0500

    Backend

--- backend/db.py
+++ backend/db.py
@@ -6,7 +6,7 @@ from mysql.connector import Error
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "1122"),
+    "password": os.getenv("DB_PASSWORD", "*****"),
     "database": os.getenv("DB_NAME", "event_management"),
 }
 
